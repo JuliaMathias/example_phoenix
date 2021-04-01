5ada3dc27def82524be85ff4d93aee49648378cb
JuliaMathias <juliamss@gmail.com>
2021-04-01 17:43:47 -0300

    Add more routes

--- lib/example_phoenix_web/router.ex
+++ lib/example_phoenix_web/router.ex
@@ -9,5 +9,9 @@ defmodule ExamplePhoenixWeb.Router do
     pipe_through :api
 
     get "/products/:products/emails/:emails", ChallengeController, :path_parameters
+    get "/challenge_by_query", ChallengeController, :challenge_by_query
+    get "/challenge_by_headers", ChallengeController, :challenge_by_headers
+
+    post "/challenge_by_post", ChallengeController, :challenge_by_query
   end
 end
